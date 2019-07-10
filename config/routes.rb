870c0b821c322c02a986b71cc881b187c6c06ac0
Pascal Rassaby <pasbynumbers@gmail.com>
2019-07-10 15:36:39 +0100

    fix schema double-check

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :planets
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: => 'welcome#home'
+
 end
