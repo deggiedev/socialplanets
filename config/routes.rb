56f85e86d71759ed3d3033480ef190ced8dda973
Pascal Rassaby <pasbynumbers@gmail.com>
2019-07-10 15:48:47 +0100

    has a basic welcome

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :planets
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: => 'welcome#home'
+  root 'welcome#home'
 
 end
