66abc0e054211ec2576aee7af12c7841255ef5a5
Pascal Rassaby <pasbynumbers@gmail.com>
2019-07-10 19:40:16 +0100

    pre-deploy ready 1

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,20 @@
 class ActivitiesController < ApplicationController
+
+  def index
+    @activities = Activity.all
+    @questions = Question.all
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+    @questions = Question.all.select {|q| q.id == @activity.id }
+    
+  end
+
+  def recieve_answer
+    byebug
+    user_answer = params[:desision]
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :resources
+  resources :questions, only: [:index]
+  resources :resources, only: [:index]
   resources :activities
-  resources :planets
-  resources :users
+  resources :planets, only: [:index]
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
+  post '/questions', to: 'questions#index'
 
 end
