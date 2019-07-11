a7686f8b1bbbe222ed057d441d6192c9e0444d0b
Pascal Rassaby <pasbynumbers@gmail.com>
2019-07-11 13:38:21 +0100

    updated with previous methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,37 @@
 class QuestionsController < ApplicationController
+ 
+  def index
+   
+  end
+
+  def show
+    @questions = Question.all.select {|q| q.activity.activity_name == params[:commit] }
+    @chosen_question = @questions.sample
+    
+  end
+
+  
+  def recieve_answer
+    @planet = Planet.find(1)
+    @question = Question.find_by_answer(params[:solution])
+    @activity = @question.activity
+    
+    byebug
+   if params[:solution] == params[:commit]
+    
+    redirect_to '/success' 
+   else
+    redirect_to '/failure'
+   end
+    
+  end
+
+  def success
+
+  end
+
+  def failure
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :materials
+  resources :questions, only: [:index, :show]
+  resources :materials, only: [:index]
   resources :activities
-  resources :planets
-  resources :users
+  resources :planets, only: [:index]
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  post '/questions', to: 'questions#show'
+  patch 'recieve_answer', to: 'questions#recieve_answer'
+  get '/success', to: 'questions#success'
+  get '/failure', to: 'questions#failure'
+
+
 end
