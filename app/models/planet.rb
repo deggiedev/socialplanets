e9389d6f53a5e86f52d1e707e9383d054acf9ac9
Jack de Grave <jack.degrave@outlook.com>
2019-07-11 15:53:24 +0100

    CSS styles for planets and users

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -4,14 +4,4 @@ class Planet < ApplicationRecord
   has_many :questions, through: :activities
   has_many :resources, through: :questions
 
-  def user_name=(name)
-    self.user = User.find_or_create_by(username: name)
-  end
-
-  def category_name
-     self.user ? self.user.name : nil
-  end
-
-  
-
 end
